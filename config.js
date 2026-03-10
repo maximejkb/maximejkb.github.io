8447f209930f70c7106a7fcdefc0823ebd56c8ec
Maxime Kawawa-Beaudan <maxime.jkb@gmail.com>
2026-03-10 18:07:36 -0400

    Adding config.

---
+++ config.js
@@ -0,0 +1,21 @@
+const SHOPIFY_DOMAIN = 'fsbyjf-0h.myshopify.com';
+const SHOPIFY_TOKEN  = '3b69edf2f3e9e0998f6bc311ff7b01f4';
+
+const WORKS = [
+  {
+    title: "Untitled I",
+    medium: "Ink on paper",
+    dimensions: '8" × 11"',
+    image: "images/work-01.jpg",
+    originalAvailable: true,
+    shopifyProductId: "REPLACE_WITH_PRODUCT_ID", // replace with each product's ID from Shopify
+  },
+  {
+    title: "Untitled II",
+    medium: "Ink on paper",
+    dimensions: '8" × 11"',
+    image: "images/work-02.jpg",
+    originalAvailable: true,
+    shopifyProductId: "REPLACE_WITH_PRODUCT_ID",
+  },
+];
